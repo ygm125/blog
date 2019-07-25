d6d319d69dcfb5cdcbc9c8b036f1acbf85edca85
yuguangming <yuguangming@meituan.com>
2019-07-25 17:47:07 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,9 @@ npm run build
 # 进入生成的文件夹
 cd ./dist
 
+# 如果是发布到自定义域名
+echo 'gmiam.com' > CNAME
 
-git init
 git add -A
 git commit -m 'deploy'
 
@@ -19,4 +20,4 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/ygm125/blog.git master:gh-pages
